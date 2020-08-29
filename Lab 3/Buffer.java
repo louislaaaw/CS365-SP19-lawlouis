677b9eb8a694267ff3dfe3a6121b0f3f354fef9f
Louis <lawyunho@gmail.com>
2020-08-29 00:02:59 -0700

    deleted all projects and updated .gitignore

--- Lab 3/Buffer.java
+++
@@ -1,4 +0,0 @@
-public interface Buffer {
-	public abstract void insert(Object item);
-	public abstract Object remove();
-}
--- Lab 3/Channel.java
+++
@@ -1,7 +0,0 @@
-public interface Channel<E> {
-	// Send a message to the channel
-	public void send(E item);
-
-	// Receive a message from the channel
-	public E receive();
-}
